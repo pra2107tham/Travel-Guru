11e11f408f0471ec828d1375c7ab2adec7c2aff4
Pratham Shirbhate <prathamshirbhate1909@gmail.com>
2024-06-25 00:34:01 +0530

    chore: Update generateToken.js to set secure flag for cookies and specify sameSite and domain

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,7 +8,9 @@ const generateTokenAndSetCookie = (userId, res) => {
         maxAge: 15*24*60*60*1000,
         // sameSite: "strict",
         secure: true,
-        // path:"/"
+        sameSite: "none",
+        domain: '.onrender.com', // Set to the parent domain that covers both frontend and backend
+        path:"/"
     })
 }
 
